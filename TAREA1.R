07c034959d73fe4d3dd75ed8c2aa5453d959477f
Dinorahh <70870504+Dinorahh@users.noreply.github.com>
2020-09-07 02:04:05 -0600

    Add files via upload

---
+++ TAREA1.R
@@ -0,0 +1,16 @@
+library(readr)
+Car
+summary(Car)
+# ******************medidas de tendencia central****************
+
+#calculando la media
+
+mean(Car$speed1)
+library(modeest)
+
+mfv(Car$type4)
+# varianza y desviacion estandar
+var(Car$pollution3)
+var(Car$pollution5)
+sd(Car$pollution3)
+sd(Car$pollution5)
